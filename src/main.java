e68c302277772c0111a74a0184039ba97d942579
JOL <joel.ortizlancharro@gmail.com>
2026-09-21 20:50:37 +0200

    feature: inici projecte

---
+++ src/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public  class main {
+
+    Scanner e = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        main p = new main();
+        p.principal();
+    }
+
+    public void principal(){
+
+        
+    }
+}
